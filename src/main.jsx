cc43662b045f571509fc0c73dc9cc3dc6fdef313
unknown <may_nofal>
2025-06-29 16:04:19 -0700

    fix: use HashRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Root from './components/Root.jsx'
 
 import Home from './components/header/Home.jsx'
 import Sign from './components/sigin/Sign.jsx'
 import SignUp from './components/sigin/SiginUp.jsx'
 
-const routes=createBrowserRouter ([{
+const routes=createHashRouter ([{
   path:'/', element:<Root/> ,children:[{
     path:'',element:<Home/>
     
